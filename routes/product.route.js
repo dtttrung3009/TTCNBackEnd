301af15665ea103006fbb829fc3c060ebcfa57f8
DangTrung24hdev <trungdang.24hdev@gmail.com>
2020-12-12 20:21:08 +0700

    edit data input

--- routes/product.route.js
+++ routes/product.route.js
@@ -6,8 +6,10 @@ const { isAdmin, isAuth } = require("../middlewares/auth.middleware");
 router.get("/", productController.getAllProduct);
 router.get("/:id", productController.getProductById);
 router.get("/:id/reviews", productController.createReviewProduct);
-router.put("/:id", isAuth, isAdmin, productController.updateInfoProduct);
-router.delete("/:id", isAuth, isAdmin, productController.deleteProduct);
-router.post("/", isAuth, isAdmin, productController.createProduct);
-
+// router.put("/:id", isAuth, isAdmin, productController.updateInfoProduct);
+router.put("/:id", productController.updateInfoProduct);
+// router.delete("/:id", isAuth, isAdmin, productController.deleteProduct);
+router.delete("/:id", productController.deleteProduct);
+// router.post("/", isAuth, isAdmin, productController.createProduct);
+router.post("/", productController.createProduct);
 module.exports = router;
