63d95196d8aff7fe56d86c3a6d716b47815a2c72
DangTrung24hdev <trungdang.24hdev@gmail.com>
2020-12-14 00:13:11 +0700

    edit feature create review for product

--- routes/product.route.js
+++ routes/product.route.js
@@ -5,7 +5,8 @@ const { isAdmin, isAuth } = require("../middlewares/auth.middleware");
 
 router.get("/", productController.getAllProduct);
 router.get("/:id", productController.getProductById);
-router.get("/:id/reviews", productController.createReviewProduct);
+// router.get("/:id/reviews", isAuth,productController.createReviewProduct);
+router.post("/:id/reviews", productController.createReviewProduct);
 // router.put("/:id", isAuth, isAdmin, productController.updateInfoProduct);
 router.put("/:id", productController.updateInfoProduct);
 // router.delete("/:id", isAuth, isAdmin, productController.deleteProduct);
